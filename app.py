54db0a077507bafa33cc2cb8391f1c6e40cc2472
MALDEME Alexandre <amaldeme@free.fr>
2017-08-01 19:27:58 +0200

    Add app.config and flash()

--- app.py
+++ app.py
@@ -9,14 +9,19 @@ from flask import render_template
 from flask import request
 from flask import make_response
 from flask import flash
+from flask import redirect
 from werkzeug import secure_filename    # secure_filemane()
 import imghdr                           # what()
 
 
 app = Flask(__name__)
-app.allowed_extensions = ('bmp', 'gif', 'png', 'jpg', 'jpeg')
-app.upload_dir = './uploads/'
+
+# App config
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024                         # 16Mb max per upload
+app.config['ALLOWED_EXTENSIONS'] = ('bmp', 'gif', 'png', 'jpg', 'jpeg')     # Allowed file extensions to be uploaded
+app.config['UPLOAD_DIR'] = './uploads/'                                     # Upload directory
 app.secret_key = '1RK+3588rZaM081C/c6fhTIvNOzb1L9K9nP0ojX3O7b7wJjAz5/I7EICH3m+/530/sW7iotaUK4R'
+
 
 # Main page
 @app.route("/index/", methods=['GET', 'POST'])
@@ -27,26 +32,34 @@ def main():
     elif request.method == 'POST':
         
         # If file is uploaded
-        if request.files['file']:
+        if request.files['file'] and request.files['file'].filename:
 
             # If the extension of the file is allowed
             if check_filetype(request.files['file'], request.files['file'].filename):
                 
                 filename = secure_filename(request.files['file'].filename)
-                request.files['file'].save('./uploads/' + filename)
+                request.files['file'].save(app.config['UPLOAD_DIR'] + filename)
                 return "OK"
+                # TODO: flash()
+                # #return redirect(request.url)
+                
             else:
                 return "File extension not allowed"
+                # TODO: flash()
+                #return redirect(request.url)
 
         else:
+            flash(u'No file uploaded !', 'error')
             return "No file uploaded"
+            # TODO: flash()
+            #return redirect(request.url)
 
 
 # Return true if the extension of filename is allowed
 # and the type is truly an image, and not a random file 
 # with an image extension
 def check_filetype(file, filename):
-    return  filename.rsplit('.')[-1].lower() in app.allowed_extensions and  imghdr.what(file)
+    return  filename.rsplit('.')[-1].lower() in app.config['ALLOWED_EXTENSIONS'] and  imghdr.what(file)
 
 
 
