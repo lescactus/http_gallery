3e04f35fbe3adf5fc248a9ca24ca6d805617ed16
MALDEME Alexandre <amaldeme@free.fr>
2017-08-12 13:48:39 +0200

    Add Flask-WTFforms

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ from flask import make_response
 from flask import flash
 from flask import redirect
 from flask import send_from_directory
-from werkzeug import secure_filename    # secure_filemane()
-import imghdr                           # what()
-import os                               # join()
+from werkzeug import secure_filename            # secure_filemane()
+from flask_wtf import FlaskForm                 # FlaskForm
+from flask_wtf.file import FileField            # FileField()
+from flask_wtf.file import FileRequired         # FileRequired()
+from wtforms.validators import ValidationError  # ValidationError()
+import imghdr                                   # what()
+import os                                       # join()
 
 
 app = Flask(__name__)
@@ -25,52 +29,73 @@ app.config['UPLOAD_DIR'] = 'uploads/'                                       # Up
 app.secret_key = '1RK+3588rZaM081C/c6fhTIvNOzb1L9K9nP0ojX3O7b7wJjAz5/I7EICH3m+/530/sW7iotaUK4R'
 
 
+
+
+
+def validate_file(alwd_ext):
+
+    def _validate_file(form, field):
+
+        # Raise a ValidationError if the image is not
+        # a real image. Ex: random file with image extension
+        if not imghdr.what(field.data) in alwd_ext:
+            message = "File must be an image !"
+            raise ValidationError(message)
+            return _validate_file
+
+        # Raise a ValidationError if the image extension is not
+        # listed in app.config['ALLOWED_EXTENSIONS']
+        if not field.data.filename.rsplit('.')[-1].lower() in app.config['ALLOWED_EXTENSIONS']:
+            message = "File must have a valid image extension !"
+            raise ValidationError(message)
+            return _validate_file
+        
+        
+    return _validate_file
+
+
+# Image form declaration
+class ImageForm(FlaskForm):
+
+    image = FileField('image', validators=[
+        FileRequired(),
+        validate_file(app.config['ALLOWED_EXTENSIONS'])
+    ])
+
+
+
 # Main page
 @app.route("/index/", methods=['GET', 'POST'])
 @app.route("/", methods=['GET', 'POST'])
 def main():
-    #if request.method == 'GET':
-    #    return render_template('index.html')
+
+    form = ImageForm()
     if request.method == 'POST':
-        
-        # If file is uploaded
-        if request.files['file'] and request.files['file'].filename:
-            
-            # If the extension and type of the file is allowed
-            if ( 
-                    check_filetype(request.files['file']) 
-                and 
-                    os.path.splitext(request.files['file'].filename)[-1].lower() 
-            ):
-                
-                filename = secure_filename(request.files['file'].filename)
 
-                # Save the file in app.config['UPLOAD_DIR']
-                request.files['file'].save(
-                    os.path.join(
-                        app.config['UPLOAD_DIR'], 
-                        filename)
-                )
+        # Form is submitted
+        if form.validate_on_submit():
+            f = form.image.data
+            filename = secure_filename(f.filename)
 
-                flash(u'OK', 'status_ok')
-                return redirect(request.url)
-                
-            else:
-                flash(u'File extension or type not allowed !', 'error')
+            print("f: " + str(f))
+            print("filename: " + str(filename))
+            print("save path: " + os.path.join(app.config['UPLOAD_DIR'],  filename))
 
-        else:
-            flash(u'No file uploaded !', 'error')
+            f.save(os.path.join(
+                app.config['UPLOAD_DIR'], 
+                filename
+            ))
+
+            flash(u'File successfully uploaded', 'status_ok')
+            return redirect(request.url)
 
     # List if images in the upload folder
     images = [ img for img in os.listdir(app.config['UPLOAD_DIR']) 
         if 
         (
-            check_filetype
-            (
-                os.path.join(app.config['UPLOAD_DIR'], img)
-            ) 
-            and 
-            img.rsplit('.')[-1].lower()
+            check_filetype(os.path.join(app.config['UPLOAD_DIR'], 
+                img)) 
+            and img.rsplit('.')[-1].lower() 
                 in app.config['ALLOWED_EXTENSIONS']
         )
     ]
@@ -78,7 +103,8 @@ def main():
     # Display the index for GET or POST requests
     return render_template('index.html', 
         upload_dir=app.config['UPLOAD_DIR'], 
-        images=images)
+        images=images,
+        form=form)
 
 
 
@@ -88,13 +114,18 @@ def uploaded_file(filename):
                                filename)
 
 
-# Return true if the type is truly an image, 
-# and not a random file # with an image 
-# extension
-def check_filetype(file):
-    return imghdr.what(file) in app.config['ALLOWED_EXTENSIONS']
-
-
-
 if __name__ == "__main__":
     app.run()
+
+
+
+# Raise a ValidationError if the image is not
+# a real image. Ex: random file with image extension
+def check_filetype(alwd_ext):
+    message = "File must be an image !"
+
+    def _check_filetype(form, field):    
+        if not imghdr.what(field.data) in alwd_ext:
+            raise ValidationError(message)
+  
+    return _check_filetype
